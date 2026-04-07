94c04504cf6d18b2a7a75533505d5842cbaaceb5
kacper0025 <kacpermaciej69@gmail.com>
2026-04-08 00:39:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,8 @@ require('dotenv').config();
 
 const app = express();
 
-// Konfiguracja EJS
 app.set('view engine', 'ejs');
 
-// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(session({
@@ -17,10 +15,8 @@ app.use(session({
     saveUninitialized: false
 }));
 
-// Połączenie z bazą
 mongoose.connect(process.env.MONGO_URI);
 
-// Routery
 app.use('/', require('./routes/auth'));
 app.use('/', require('./routes/clicker'));
 
